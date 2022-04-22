7fffa30fd65d8b47c3759c519645f575e88292ac
dirkarnez <smalldirkalex@gmail.com>
2022-04-23 02:53:15 +0800

    - add repository origin auto detection

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/dirkarnez/git-commit-helper
 
 go 1.17
+
+require (
+	github.com/fatih/color v1.13.0
+	github.com/graniticio/inifile v0.0.0-20210325083046-86e94fdc8f99
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- main.go
+++ main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/fs"
 	"log"
+	"net/url"
 	"path/filepath"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/graniticio/inifile"
 )
 
 var (
 	dir string
+	c   = color.New(color.FgCyan)
 )
 
-func Scan(root string) {
+func Scan(root string, callback func(s string, d fs.DirEntry) error) {
 	filepath.WalkDir(root, func(s string, d fs.DirEntry, e error) error {
 		if e != nil {
-			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", s, e)
+			c.Printf("prevent panic by handling failure accessing a path %q: %v\n", s, e)
 			return e
 		}
 
@@ -24,28 +29,23 @@ func Scan(root string) {
 		// 	return filepath.SkipDir
 		// }
 
-		fileInfo, err := d.Info()
-		if err != nil {
-			return err
-		}
-
-		size := fileInfo.Size()
-		if size > 104857600 { // 1024 * 1024
-			fmt.Println("big file: ", s)
-		}
-
 		if d.IsDir() {
 			for _, BLACKLISTED_FOLDER_NAME := range BLACKLISTED_FOLDER_NAMES {
 				if d.Name() == BLACKLISTED_FOLDER_NAME {
-					fmt.Println("blacklisted folder: ", s)
+					c.Println("blacklisted folder: ", s)
 				}
 			}
 		}
 
 		for _, BLACKLISTED_FILE_EXTENSION := range BLACKLISTED_FILE_EXTENSIONS {
 			if filepath.Ext(d.Name()) == BLACKLISTED_FILE_EXTENSION {
-				fmt.Println("file with blacklisted extension: ", s)
+				c.Println("file with blacklisted extension: ", s)
 			}
+		}
+
+		err := callback(s, d)
+		if err != nil {
+			return err
 		}
 
 		return nil
@@ -65,5 +65,35 @@ func main() {
 		log.Fatal("No --dir is given")
 	}
 
-	Scan(dir)
+	ic, _ := inifile.NewIniConfigFromPath(filepath.Join(dir, ".git", "config"))
+	originURL, _ := ic.Value("remote \"origin\"", "url")
+	if len(originURL) > 0 {
+		u, _ := url.Parse(originURL)
+		parts := strings.Split(u.Hostname(), ".")
+		domain := parts[len(parts)-2] + "." + parts[len(parts)-1]
+
+		switch {
+		case domain == "github.com":
+			c.Println("GitHub repository detected...")
+
+			Scan(dir, func(s string, d fs.DirEntry) error {
+				fileInfo, err := d.Info()
+				if err != nil {
+					return err
+				}
+
+				size := fileInfo.Size()
+				if size > 104857600 { // 1024 * 1024
+					c.Println("big file: ", s)
+				}
+				return nil
+			})
+
+		case domain == "bitbucket.com":
+			Scan(dir, func(s string, d fs.DirEntry) error {
+				return nil
+			})
+		}
+	}
+
 }
