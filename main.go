9aa921ea6f25a163e56a60a8ffdc29ad5427689d
dirkarnez <smalldirkalex@gmail.com>
2022-04-26 21:32:49 +0800

    - upload files

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/fs"
 	"log"
 	"net/url"
@@ -17,8 +18,8 @@ var (
 	c   = color.New(color.FgCyan)
 )
 
-func Scan(root string, callback func(s string, d fs.DirEntry) error) {
-	filepath.WalkDir(root, func(s string, d fs.DirEntry, e error) error {
+func Scan(root string, callback func(s string, d fs.DirEntry) error) error {
+	return filepath.WalkDir(root, func(s string, d fs.DirEntry, e error) error {
 		if e != nil {
 			c.Printf("prevent panic by handling failure accessing a path %q: %v\n", s, e)
 			return e
@@ -32,14 +33,26 @@ func Scan(root string, callback func(s string, d fs.DirEntry) error) {
 		if d.IsDir() {
 			for _, BLACKLISTED_FOLDER_NAME := range BLACKLISTED_FOLDER_NAMES {
 				if d.Name() == BLACKLISTED_FOLDER_NAME {
-					c.Println("blacklisted folder: ", s)
+					return fmt.Errorf("blacklisted folder: %s", s)
 				}
 			}
-		}
 
-		for _, BLACKLISTED_FILE_EXTENSION := range BLACKLISTED_FILE_EXTENSIONS {
-			if filepath.Ext(d.Name()) == BLACKLISTED_FILE_EXTENSION {
-				c.Println("file with blacklisted extension: ", s)
+			for _, WARNING_FOLDER_NAME := range BLACKLISTED_FOLDER_NAMES {
+				if d.Name() == WARNING_FOLDER_NAME {
+					c.Println("warning folder: ", s)
+				}
+			}
+		} else {
+			for _, BLACKLISTED_FILE_EXTENSION := range BLACKLISTED_FILE_EXTENSIONS {
+				if filepath.Ext(d.Name()) == BLACKLISTED_FILE_EXTENSION {
+					return fmt.Errorf("file with blacklisted extension: %s", s)
+				}
+			}
+
+			for _, WARNING_FILE_EXTENSION := range BLACKLISTED_FILE_EXTENSIONS {
+				if filepath.Ext(d.Name()) == WARNING_FILE_EXTENSION {
+					c.Println("file with warning extension: ", s)
+				}
 			}
 		}
 
@@ -53,8 +66,10 @@ func Scan(root string, callback func(s string, d fs.DirEntry) error) {
 }
 
 var (
-	BLACKLISTED_FILE_EXTENSIONS = []string{".url", ".log", ".DS_Store"}
-	BLACKLISTED_FOLDER_NAMES    = []string{"node_modules", ".tmp.driveupload", ".git", "vendor", "tmp", "__MACOSX"}
+	WARNING_FILE_EXTENSIONS     = []string{".url"}                                                          // maybe useful in some case
+	WARNING_FOLDER_NAMES        = []string{".git"}                                                          // maybe its .git folder
+	BLACKLISTED_FILE_EXTENSIONS = []string{".log", ".DS_Store"}                                             // to be deleted
+	BLACKLISTED_FOLDER_NAMES    = []string{"node_modules", ".tmp.driveupload", "vendor", "tmp", "__MACOSX"} // to be deleted
 )
 
 func main() {
@@ -65,35 +80,43 @@ func main() {
 		log.Fatal("No --dir is given")
 	}
 
+	var domain = "github.com"
+
 	ic, _ := inifile.NewIniConfigFromPath(filepath.Join(dir, ".git", "config"))
-	originURL, _ := ic.Value("remote \"origin\"", "url")
-	if len(originURL) > 0 {
-		u, _ := url.Parse(originURL)
-		parts := strings.Split(u.Hostname(), ".")
-		domain := parts[len(parts)-2] + "." + parts[len(parts)-1]
-
-		switch {
-		case domain == "github.com":
-			c.Println("GitHub repository detected...")
-
-			Scan(dir, func(s string, d fs.DirEntry) error {
-				fileInfo, err := d.Info()
-				if err != nil {
-					return err
-				}
-
-				size := fileInfo.Size()
-				if size > 104857600 { // 1024 * 1024
-					c.Println("big file: ", s)
-				}
-				return nil
-			})
-
-		case domain == "bitbucket.com":
-			Scan(dir, func(s string, d fs.DirEntry) error {
-				return nil
-			})
+	if ic != nil {
+		originURL, _ := ic.Value("remote \"origin\"", "url")
+		if len(originURL) > 0 {
+			u, _ := url.Parse(originURL)
+			parts := strings.Split(u.Hostname(), ".")
+			domain = parts[len(parts)-2] + "." + parts[len(parts)-1]
 		}
 	}
 
+	var err error
+	switch {
+	case domain == "github.com":
+		c.Println("GitHub repository detected...")
+
+		err = Scan(dir, func(s string, d fs.DirEntry) error {
+			fileInfo, err := d.Info()
+			if err != nil {
+				return err
+			}
+
+			size := fileInfo.Size()
+			if size > 104857600 { // 1024 * 1024
+				return fmt.Errorf("big file: %s", s)
+			}
+			return nil
+		})
+
+	case domain == "bitbucket.com":
+		err = Scan(dir, func(s string, d fs.DirEntry) error {
+			return nil
+		})
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
